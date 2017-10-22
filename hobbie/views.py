3eb6ff5e8dbe5463ccbb1d8010f00c97f84b9aa1
laser <avi.upsale@gmail.com>
2017-10-22 13:59:34 +0300

    post fix

--- hobbie/views.py
+++ hobbie/views.py
@@ -4,7 +4,6 @@ from django.views.generic import TemplateView
 from hobbie.models import Card, MyTrack
 from django.views.decorators.csrf import csrf_exempt
 import json
-from django_mako_plus import view_function
 from django.conf import settings
 
 def home(request):
@@ -21,17 +20,17 @@ def mymap(request):
 
 @csrf_exempt
 def savetrack(request):
-    if request.is_ajax():
-        if request.method == 'POST':
-           #print(json.loads(request.body.decode('utf-8')))
-            trackdata = json.loads(request.body.decode('utf-8'))
-            mt = MyTrack()
-            mt.username = trackdata['user']
-            mt.track = json.dumps(trackdata['track'])
-            mt.trackname = trackdata['name']
-            mt.description = trackdata['description']
-            mt.save()
-            print('saved!')
+    #if request.is_ajax():
+    if request.method == 'POST':
+       #print(json.loads(request.body.decode('utf-8')))
+        trackdata = json.loads(request.body.decode('utf-8'))
+        mt = MyTrack()
+        mt.username = trackdata['user']
+        mt.track = json.dumps(trackdata['track'])
+        mt.trackname = trackdata['name']
+        mt.description = trackdata['description']
+        mt.save()
+        print('saved!')
     return HttpResponse("OK")
 
 
@@ -40,8 +39,3 @@ def gettrack(request):
     tracks = MyTrack.objects.filter(username=user).order_by("created_date")
     return render(request, 'hobbie/gettrackvue.html', {'tracks': tracks})
 
-
-@view_function
-def tes(request):
-    user = request.user
-    return request.dmp_render
